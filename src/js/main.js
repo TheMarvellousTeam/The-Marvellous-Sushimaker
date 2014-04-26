febbc9cfc6ca45b9d1944cdd17971915b9f9585d
platane <arthur@arthur-brongniart.fr>
2014-04-26 14:44:42 +0200

    go back to phaser

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,16 @@
-var PIXI = require('PIXI');
 
-// bootstrap PIXI engine
+// bootstrap Phaser
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+ 
+function preload() {
+	game.load.image('boat', 'src/img/boat.png');
+}
+ 
+function create() {
 
+	game.add.sprite(0, 0, 'boat');
+
+}
+ 
+function update() {
+}
