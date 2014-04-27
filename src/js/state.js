3f31fdeba33906326f1f5263e4c779e6962bc451
NiZiL <biasutto.t@gmail.com>
2014-04-27 05:29:20 +0200

    chalutier debug + deplacement test :o)

--- src/js/state.js
+++ src/js/state.js
@@ -3,73 +3,64 @@ var GameState = GameState || {};
 (function( exposure ){
 
 	var BootState = new Phaser.State();
-		BootState.preload = function() {
-  		this.load.image('logo', 'src/assets/logo.png');
-  		this.load.image('baleine', 'src/assets/baleine.png');
-  		this.load.image('banc_poissons', 'src/assets/banc_poissons.png');
-  		this.load.image('chalutier_up', 'src/assets/chalutier_up.png');
-  		this.load.image('chalutier_down', 'src/assets/chalutier_down.png');
-  		this.load.image('dauphin', 'src/assets/dauphin.png');
-  		this.load.image('iceberg', 'src/assets/iceberg.png');
-  		this.load.image('mine', 'src/assets/mine.png');
-  		this.load.image('sea_shepher', 'src/assets/sea_shepherd.png');
-  		this.load.image('sushi', 'src/assets/sushi.png');
+	BootState.preload = function() {
+  	this.load.image('logo', 'src/assets/logo.png');
+  	this.load.image('baleine', 'src/assets/baleine.png');
+  	this.load.image('banc_poissons', 'src/assets/banc_poissons.png');
+  	this.load.image('chalutier_up', 'src/assets/chalutier_up.png');
+  	this.load.image('chalutier_down', 'src/assets/chalutier_down.png');
+    this.load.image('dauphin', 'src/assets/dauphin.png');
+  	this.load.image('iceberg', 'src/assets/iceberg.png');
+  	this.load.image('mine', 'src/assets/mine.png');
+  	this.load.image('sea_shepher', 'src/assets/sea_shepherd.png');
+  	this.load.image('sushi', 'src/assets/sushi.png');
 	};
 	BootState.create = function() {
-  		var logo = this.add.sprite(0,0,'logo');
-  		logo.inputEnabled = true ;
-  		logo.input.useHandCurse = true;
-  		logo.events.onInputDown.add(function(){
-    		//this.game.state.start('game');
-  		});
-
-      pe = ( new components.PathEditable() ).init().listen( true );
+  	var logo = this.add.sprite(0,0,'logo');
+  	logo.inputEnabled = true ;
+  	logo.input.useHandCurse = true;
+  	logo.events.onInputDown.add(function(){
+    	this.game.state.start('game');
+  	});
 	};
-
-	BootState.update = function() {
-    pe.update();
-	};
-
 
 	var MainState = new Phaser.State();
-	MainState.preload = function() {
-
-	};
-
 	MainState.create = function() {
-  		this.stage.backgroundColor='#A5CEF2';
+    this.stage.backgroundColor='#A5CEF2';
 
 		this.world.setBounds(0, 0, 5000, 5000);
 
 		this.fish = [];
-  		for(var i=0; i<10; i++){
-  			this.createFish();
-  		}
+  	for(var i=0; i<10; i++){
+  		this.createFish();
+  	}
 
-  		this.chalutier = this.add.sprite(this.world.width/2, this.world.height/2, 'chalutier_up');
-  		this.chalutier.anchor.setTo(0.5, 0.5);
-  		this.chalutier.scale.setTo(0.3, 0.3);
-  		this.physics.enable(this.chalutier, Phaser.Physics.ARCADE);
-  		this.filetUp = true ;
-  		this.speed = 0 ;
+  	this.chalutier = this.add.sprite(this.world.width/2, this.world.height/2, 'chalutier_up');
+  	this.chalutier.anchor.setTo(0.5, 0.5);
+  	this.chalutier.scale.setTo(0.3, 0.3);
+  	this.physics.enable(this.chalutier, Phaser.Physics.ARCADE);
+  	this.filetUp = true ;
+  	this.speed = 0 ;
 		this.camera.follow(this.chalutier);
 
 		var sushi = this.add.sprite(35, this.game.height - 35, 'sushi');
-  		sushi.anchor.setTo(0.5, 0.5);
-  		sushi.scale.setTo(0.1, 0.1);
-  		sushi.fixedToCamera = true;
-  		this.sushi = this.add.text(65, this.game.height - 47, '0', {fontSize: 14, fill:"#000000"});
-  		this.sushi.fixedToCamera = true;
+  	sushi.anchor.setTo(0.5, 0.5);
+  	sushi.scale.setTo(0.1, 0.1);
+  	sushi.fixedToCamera = true;
+  	this.sushi = this.add.text(65, this.game.height - 47, '0', {fontSize: 14, fill:"#000000"});
+  	this.sushi.fixedToCamera = true;
 
-  		var spacebar = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-  		spacebar.onDown.add(function() {
-    		if (this.filetUp){
-      			this.chalutier.loadTexture('chalutier_down');
-    		} else {
-      			this.chalutier.loadTexture('chalutier_up');
-    		}
-    		this.filetUp = !this.filetUp;
-  		}, this);
+  	var spacebar = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  	spacebar.onDown.add(function() {
+      if (this.filetUp){
+      	this.chalutier.loadTexture('chalutier_down');
+      } else {
+      	this.chalutier.loadTexture('chalutier_up');
+      }
+      this.filetUp = !this.filetUp;
+  	}, this);
+
+    pe = ( new components.PathEditable() ).init().listen( true );
 	};
 
 	MainState.createFish = function() {
@@ -81,27 +72,30 @@ var GameState = GameState || {};
 	};
 
 	MainState.update = function() {
-  		if (this.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-    		this.chalutier.angle -= 2;
-  		} else if (this.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-    		this.chalutier.angle += 2;
-  		} else if (this.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-    		this.speed += 10 ;
-  		} else if (this.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-    		this.speed -= 10;
-  		}
-  		this.physics.arcade.velocityFromAngle(this.chalutier.angle, speed, this.chalutier.body.velocity);
+    pe.update();
 
+  	if (this.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+    	this.chalutier.angle -= 2;
+  	} else if (this.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+    	this.chalutier.angle += 2;
+  	} else if (this.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+      if( this.speed < 100)
+        this.speed += 10 ;
+  	} else if (this.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+    	if( this.speed > 0)
+        this.speed -= 10;
+  	}
+  	this.physics.arcade.velocityFromAngle(this.chalutier.angle, this.speed, this.chalutier.body.velocity);
 
-  		if ( this.chalutier.x > this.world.width || this.chalutier.x < 0 || this.chalutier.y > this.world.height || this.chalutier.y < 0 ){
-    		this.game.state.start('boot'); 
-  		}
+ 		if ( this.chalutier.x > this.world.width || this.chalutier.x < 0 || this.chalutier.y > this.world.height || this.chalutier.y < 0 ){
+   		this.game.state.start('boot'); 
+ 		}
 
-  		this.physics.arcade.overlap(this.chalutier, this.fish, onCollideFish, null, this);
-  		for( var i=0; i<this.fish.length; i++){
-  			this.fish[i].angle += this.fish[i].addToAngle ;
-  			this.physics.arcade.velocityFromAngle(this.fish[i].angle, 150+Math.random()*300, this.fish[i].body.velocity);
-  		}
+ 		this.physics.arcade.overlap(this.chalutier, this.fish, onCollideFish, null, this);
+ 		for( var i=0; i<this.fish.length; i++){
+ 			this.fish[i].angle += this.fish[i].addToAngle ;
+ 			this.physics.arcade.velocityFromAngle(this.fish[i].angle, 150+Math.random()*300, this.fish[i].body.velocity);
+ 		}      
 	};
 
 	var onCollideFish = function(obj1, obj2) {
