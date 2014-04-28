09d62fa6809be8322a1651bc24a249bef8429dc9
NiZiL <biasutto.t@gmail.com>
2014-04-28 16:14:43 +0200

    test on loader 2

--- src/js/state/bootstate.js
+++ src/js/state/bootstate.js
@@ -9,12 +9,12 @@ var GameState = GameState || {};
 	};
 	BootState.create = function() {
   	var logo = this.add.sprite(0,0,'logo');
-    this.game.state.start('game');
-  	// logo.inputEnabled = true ;
-  	// logo.input.useHandCurse = true;
-  	// logo.events.onInputDown.add(function(){
-   //  	this.game.state.start('game');
-  	// });
+    //this.game.state.start('game');
+  	logo.inputEnabled = true ;
+  	logo.input.useHandCurse = true;
+  	logo.events.onInputDown.add(function(){
+    	this.game.state.start('game');
+  	});
 	};
 
 	exposure.BootState = BootState;
